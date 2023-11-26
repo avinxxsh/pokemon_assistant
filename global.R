60125a66217874b3c69f1f9d6896cd05815c6f09
avinxxsh <52997831+avinxxsh@users.noreply.github.com>
2023-11-26 18:41:08 -0500

    Update global.R

--- global.R
+++ global.R
@@ -1,3 +1,4 @@
 if (!require("shiny")) install.packages("shiny")
 if (!require("shinythemes")) install.packages("shinythemes")
 if (!require("reshape2")) install.packages("reshape2")
+if (!require("DT")) install.packages("DT")
