4423e537443d5d122768a37726743c13620c8d45
avinxxsh <52997831+avinxxsh@users.noreply.github.com>
2023-11-29 22:11:52 -0500

    Update global.R

--- global.R
+++ global.R
@@ -1,4 +1,6 @@
 if (!require("shiny")) install.packages("shiny")
+if (!require("tidyverse")) install.packages("tidyverse")
 if (!require("shinythemes")) install.packages("shinythemes")
 if (!require("reshape2")) install.packages("reshape2")
 if (!require("DT")) install.packages("DT")
+
